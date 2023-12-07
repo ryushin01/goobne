9e3e70a765286b1e325e256c21424e71502e4506
Leechengwon <leecwee8497@gmail.com>
2023-12-07 10:57:39 +0900

    오타수정

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -11,7 +11,7 @@ import { ReactComponent as List } from '../../svg/HeaderListIconBtn.svg';
  * IconButton props list
  * @property {string} type: button, submit, reset                                   - 버튼 타입을 정의합니다.
  * @property {string} size: small, medium, large                                    - 버튼 크기를 정의합니다.
- * @property {string} size: black, white                                        - svgIcon 색상을 정의합니다.
+ * @property {string} color: black, white                                        - svgIcon 색상을 정의합니다.
  * @property {string} content: blog,facebook ,instagram, youtube, cart, close, list - 버튼 내부 svgicon 컴포넌트를 정의합니다.
  * @property {function} onClick                                                     - 버튼 클릭 시 실행할 함수를 위해 미리 정의합니다.
  */
