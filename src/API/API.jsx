9ef57482b93f4239be3238fd9477fea1836e1755
박요진 <syon013@naver.com>
2023-12-07 15:16:19 +0900

    ⚡️ axios를 Custom으로 사용하기 위해  API 폴더 및 파일 생성

---
+++ src/API/API.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// Custom axios를 만들기 위해 함수 생성
+
+/**
+ * BigBanner에 대한 Data를 받아오기 위한 Custom Axios
+ * @property 1. export로 다른 페이지에서 불러와 사용할 수 있도록 함.
+ * @property 2. axios.cerate로 새로운 axios를 만들어 사용함.
+ * @property 3. baseURL은 서버의 주소를 의미한다. (json파일을 받아오기 때문에 상대경로로 설정)
+ * @property 4. headers는 서버에 요청을 보낼 때 헤더에 토큰을 담아 보내기 위해 사용한다
+ */
+export const BigBannerAxios = axios.create({
+  baseURL: '../goobne/data/BigBannerData.json',
+  headers: {
+    Authorization: localStorage.getItem('token'),
+  },
+});
