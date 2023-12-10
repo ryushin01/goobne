1ac13234d437e416e05cf2ccb994ac17962c2b51
kgy8987 <rlarldud8987@naver.com>
2023-12-11 00:29:24 +0900

    :lipstick: 1차 인풋생성

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,46 @@
 import styled from 'styled-components';
 
-const Input = () => {
-  return <></>;
+const Input = ({
+  type = 'text',
+  placeholder,
+  onChange,
+  name,
+  value,
+  ...props
+}) => {
+  return (
+    <DefaultInput
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+      name={name}
+      value={value}
+      {...props}
+    />
+  );
 };
+
+/**
+ *@property {string} type: text, password              - 인풋의 타입을 정의합니다.
+ *@property {string} placeholder                       - 인풋의 입력될 값에 대한 짧은 힌트를 정의합니다.
+ *@property {string} name                              - 인풋의 form을 제출시 서버에서 데이터를 참조하기 위하여 정의합니다.
+ *@property {any} value                                - 인풋의 초깃값을 정의합니다.
+ *@property {function} onChange                        - 인풋의 값이 변경될시 실행할 함수를 정의합니다.
+ */
+
+//Default input속성값을 정의합니다.
+const DefaultInput = styled.input`
+  width: 100%;
+  padding: 13px 0px 13px 15px;
+  border: 1px solid ${props => props.theme.grayscaleF};
+  border-radius: 5px;
+  font-size: 16px;
+  outline: none;
+
+  &:focus {
+    border-color: ${props => props.theme.primaryColor};
+    box-shadow: 0 0 0 1px ${props => props.theme.primaryColor} inset;
+  }
+`;
 
 export default Input;
---
+++ src/pages/Test.jsx
@@ -0,0 +1,13 @@
+import Input from '../components/Input/Input';
+
+const Test = () => {
+  return (
+    <div>
+      <Input type="text" placeholder="디폴트 인풋" />
+      <Input type="text" placeholder="테스트1" />
+      <Input type="text" placeholder="테스트2" />
+    </div>
+  );
+};
+
+export default Test;
