794a240264c3d09e47bb37465f71624dca51532d
Minjae Park <jayy.intheworld@gmail.com>
2023-12-11 16:29:54 +0900

    :lipstick: 리뷰 내용 반영

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as ModalClose } from '../../svg/ModalClose.svg';
 
@@ -6,31 +6,13 @@ import { ReactComponent as ModalClose } from '../../svg/ModalClose.svg';
  * Modal props list
  * @property {string} title                          - 해당 모달의 제목
  * @property {string} desc                           - 해당 모달의 내용
- * @property {boolean} closeBtn                      - 모달 닫기 버튼의 표시 여부
+ * @property {boolean} isCloseBtn                      - 모달 닫기 버튼의 표시 여부
  * @property {string} size: small, medium, large     - 모달창의 크기
  */
 
-const Modal = ({ title, desc, closeBtn, ...props }) => {
+const Modal = ({ title, content, isCloseBtn, ...props }) => {
   // 모달을 여닫기 위한 state
   const [isModalOpen, setIsModalOpen] = useState(true);
-  const modalRef = useRef(null);
-
-  // 모달 외부 클릭시 모달 닫기
-  useEffect(() => {
-    // 모달이 열려있고, 클릭한 부분이 모달 내부가 아닐 때 closeModal 함수를 실행
-    const handleDimClick = e => {
-      if (modalRef.current && !modalRef.current.contains(e.target)) {
-        closeModal();
-      }
-    };
-    // 모달이 열려있을 때 mousedown를 감지하여 handleDimClick 함수를 실행
-    if (isModalOpen) {
-      window.addEventListener('mousedown', handleDimClick);
-    }
-    return () => {
-      window.removeEventListener('mousedown', handleDimClick);
-    };
-  }, [isModalOpen]);
 
   // esc 누르면 모달 닫기
   useEffect(() => {
@@ -56,17 +38,15 @@ const Modal = ({ title, desc, closeBtn, ...props }) => {
     <>
       {isModalOpen && (
         <ModalMain>
-          <ModalDim />
-          <ModalWrap ref={modalRef} {...props}>
-            <ModalContainer>
-              <ModalButton>
-                {closeBtn && <ModalClose onClick={closeModal}></ModalClose>}
-              </ModalButton>
-              <ModalContent>
-                <ModalTitle>{title}</ModalTitle>
-                <ModalDesc>{desc}</ModalDesc>
-              </ModalContent>
-            </ModalContainer>
+          <ModalDim onClick={closeModal} />
+          <ModalWrap {...props}>
+            <ModalButton>
+              {isCloseBtn && <ModalClose onClick={closeModal}></ModalClose>}
+            </ModalButton>
+            <ModalBox>
+              <ModalTitle>{title}</ModalTitle>
+              <ModalContent>{content}</ModalContent>
+            </ModalBox>
           </ModalWrap>
         </ModalMain>
       )}
@@ -78,16 +58,16 @@ export default Modal;
 
 const MODAL_SIZE = {
   small: {
-    padding: '15px',
-    borderRadius: '5px',
+    width: '460px',
+    padding: '16px',
   },
   medium: {
+    width: '600px',
     padding: '25px',
-    borderRadius: '5px',
   },
   large: {
-    padding: '0',
-    borderRadius: '20px',
+    width: '866px',
+    padding: '15px',
   },
 };
 
@@ -114,42 +94,40 @@ const ModalDim = styled.div`
 `;
 
 const ModalWrap = styled.div`
-  ${FlexCenter}
+  width: ${({ size }) => MODAL_SIZE[size]?.width || '500px'};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   border: 1px solid;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   position: fixed;
   padding: ${({ size }) => MODAL_SIZE[size]?.padding || '20px'};
-  border-radius: ${({ size }) => MODAL_SIZE[size]?.borderRadius || '5px'};
+  border-radius: 5px;
   background-color: ${props => props.theme.grayscaleA};
   z-index: 20;
 `;
 
-const ModalContainer = styled.div`
-  width: 100%;
-  margin: auto;
-`;
-
-const ModalButton = styled.div`
+const ModalButton = styled.p`
   display: flex;
-  align-items: center;
   justify-content: flex-end;
 `;
 
-const ModalContent = styled.div`
+const ModalBox = styled.div`
   ${FlexCenter}
   flex-direction: column;
+  width: 100%;
 `;
 
-const ModalTitle = styled.div`
+const ModalTitle = styled.span`
   padding-bottom: 15px;
   text-align: center;
   font-size: 18px;
   font-weight: 800;
 `;
 
-const ModalDesc = styled.div`
+const ModalContent = styled.div`
   padding: 15px;
   font-size: 14px;
 `;
