470bff27363383888127a2523fdf81b596ed7b0d
Minjae Park <jayy.intheworld@gmail.com>
2023-12-11 15:14:55 +0900

    :lipstick: 주석 및 cleanup 함수 추가

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,24 +11,23 @@ import { ReactComponent as ModalClose } from '../../svg/ModalClose.svg';
  */
 
 const Modal = ({ title, desc, closeBtn, ...props }) => {
+  // 모달을 여닫기 위한 state
   const [isModalOpen, setIsModalOpen] = useState(true);
   const modalRef = useRef(null);
 
+  // 모달 외부 클릭시 모달 닫기
   useEffect(() => {
-    const handleDimClick = event => {
-      // 모달 외부를 클릭시 모달 닫기
-      if (modalRef.current && !modalRef.current.contains(event.target)) {
+    // 모달이 열려있고, 클릭한 부분이 모달 내부가 아닐 때 closeModal 함수를 실행
+    const handleDimClick = e => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
         closeModal();
       }
     };
-
-    // 모달이 열려있을 때 EventListener를 등록
+    // 모달이 열려있을 때 mousedown를 감지하여 handleDimClick 함수를 실행
     if (isModalOpen) {
       window.addEventListener('mousedown', handleDimClick);
     }
-
     return () => {
-      // 언마운트되면 EventListener를 해제.
       window.removeEventListener('mousedown', handleDimClick);
     };
   }, [isModalOpen]);
@@ -36,11 +35,16 @@ const Modal = ({ title, desc, closeBtn, ...props }) => {
   // esc 누르면 모달 닫기
   useEffect(() => {
     const esc = e => {
+      // Escape(esc)키가 눌리면 closeModal 함수를 실행
       if (e.key === 'Escape') {
         closeModal();
       }
     };
+    // keydown event를 감지하여 esc 함수를 실행
     window.addEventListener('keydown', esc);
+    return () => {
+      window.removeEventListener('keydown', esc);
+    };
   }, []);
 
   // 모달 닫기시 실행되는 함수
@@ -58,8 +62,10 @@ const Modal = ({ title, desc, closeBtn, ...props }) => {
               <ModalButton>
                 {closeBtn && <ModalClose onClick={closeModal}></ModalClose>}
               </ModalButton>
-              <ModalTitle>{title}</ModalTitle>
-              <ModalDesc>{desc}</ModalDesc>
+              <ModalContent>
+                <ModalTitle>{title}</ModalTitle>
+                <ModalDesc>{desc}</ModalDesc>
+              </ModalContent>
             </ModalContainer>
           </ModalWrap>
         </ModalMain>
@@ -71,11 +77,15 @@ const Modal = ({ title, desc, closeBtn, ...props }) => {
 export default Modal;
 
 const MODAL_SIZE = {
-  map: {
-    padding: '15px 20px',
+  small: {
+    padding: '15px',
     borderRadius: '5px',
   },
-  main: {
+  medium: {
+    padding: '25px',
+    borderRadius: '5px',
+  },
+  large: {
     padding: '0',
     borderRadius: '20px',
   },
@@ -105,8 +115,6 @@ const ModalDim = styled.div`
 
 const ModalWrap = styled.div`
   ${FlexCenter}
-  width: 100%;
-  max-width: 500px;
   border: 1px solid;
   top: 50%;
   left: 50%;
@@ -119,13 +127,19 @@ const ModalWrap = styled.div`
 `;
 
 const ModalContainer = styled.div`
-  width: 90%;
+  width: 100%;
   margin: auto;
 `;
 
 const ModalButton = styled.div`
   display: flex;
+  align-items: center;
   justify-content: flex-end;
+`;
+
+const ModalContent = styled.div`
+  ${FlexCenter}
+  flex-direction: column;
 `;
 
 const ModalTitle = styled.div`
