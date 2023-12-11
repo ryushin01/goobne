7e3be485a88a86acc6b4d0d3f2b4fef4bdcc8003
kgy8987 <rlarldud8987@naver.com>
2023-12-11 21:14:47 +0900

    :art::lipstick: input 컴포넌트 구조개선 및 UI작업

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -8,13 +8,17 @@ const Input = ({
   value,
   label,
   labelForId,
-  before,
+  position,
+  required,
   ...props
 }) => {
   return (
-    <InputWrap>
-      <InputText before={before} htmlFor={labelForId}>
-        {label}
+    <InputWrap position={position}>
+      <InputText htmlFor={labelForId} isRequired={required}>
+        {label}{' '}
+        {required === 'required' && (
+          <span style={{ color: 'red' }}>&nbsp;*</span>
+        )}
       </InputText>
       <DefaultInput
         type={type}
@@ -34,19 +38,37 @@ const Input = ({
  *@property {string} placeholder                       - 인풋의 입력될 값에 대한 짧은 힌트를 정의합니다.
  *@property {string} name                              - 인풋의 form을 제출시 서버에서 데이터를 참조하기 위하여 정의합니다.
  *@property {any} value                                - 인풋의 초깃값을 정의합니다.
+ *@property {any} position: column, 기본값 = row        - 인풋의 Wrap의 포지션을 해당 조건에따라 적용하는것을 정의합니다.
  *@property {function} onChange                        - 인풋의 값이 변경될시 실행할 함수를 정의합니다.
  */
 
+//INPUT WRAP position에따른 속성값.
+const INPUT_WRAP_POSITION_STYLES = {
+  column: {
+    flexDirection: 'column',
+  },
+};
+
 const InputWrap = styled.div`
   display: flex;
+
+  /* props position이 전달되지 않은 경우 기본설정에 따르고, 
+  props를 전달 받으면 지정조건에 따른다 */
+  flex-direction: ${({ position }) =>
+    INPUT_WRAP_POSITION_STYLES[position]?.flexDirection || 'row'};
 `;
+
 const InputText = styled.label`
-  display: block;
-  margin-bottom: 8px;
+  display: flex;
+  align-items: center;
+  width: 200px;
   font-size: 14px;
   font-weight: 700;
+  padding: 0px 0px 0px 10px;
+  position: relative;
 
-  &[before] {
+  //flex-direction row의 기본값에서 Label의 앞쪽 before속성을 의미한다.
+  &::before {
     content: '';
     display: inline-block;
     position: absolute;
