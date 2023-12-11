b6b4a398a4335a7cb56759c3cbd7532194b89ee2
Minjae Park <jayy.intheworld@gmail.com>
2023-12-11 19:23:11 +0900

    :lipstick: portal 주석 추가

--- src/components/Modal/Portal.jsx
+++ src/components/Modal/Portal.jsx
@@ -1,7 +1,9 @@
 import ReactDom from 'react-dom';
 
 const Portal = ({ children }) => {
+  // modal이 자리한 DOM 위치를 el이라는 변수를 사용하여 정의
   const el = document.getElementById('modal');
+  // createPortal 메서드를 사용하여 children을 el에서 렌더링
   return ReactDom.createPortal(children, el);
 };
 
