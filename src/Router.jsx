87a746f3773755c6d03a9a123b5662c9000ded45
kgy8987 <rlarldud8987@naver.com>
2023-12-12 14:10:39 +0900

    :construction: 브랜치이동으로 인한 임시커밋

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 // import Test from './pages/Test';
 import Herder from './components/Header/Header';
+import Order from './pages/Order';
 const Router = () => {
   return (
     <BrowserRouter basename="/goobne">
       <Herder />
-      <Routes>{/* <Route path="/" element={<Test />} /> */}</Routes>
+      <Routes>
+        {/* <Route path="/" element={<Test />} /> */}
+        <Route path="/order" element={<Order />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ src/pages/Order.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const Order = () => {
+  return (
+    <OrderContainer>
+      <OrderContentWrap>
+        <OrderTitle>
+          <span>결제하기</span>
+        </OrderTitle>
+        <form>
+          <fieldset>
+            <legend>배달정보</legend>
+          </fieldset>
+        </form>
+      </OrderContentWrap>
+    </OrderContainer>
+  );
+};
+
+export default Order;
+
+const OrderContainer = styled.main`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding-top: 170px;
+`;
+
+const OrderContentWrap = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  background-color: #a6a6a6;
+`;
+
+const OrderTitle = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-bottom: 20px;
+
+  & > span {
+    font-size: 40px;
+    font-weight: 800;
+  }
+`;
