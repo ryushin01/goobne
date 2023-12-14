9b3cf856b693b2b905f9393f5d7b1f5e94d548e5
박요진 <syon013@naver.com>
2023-12-14 20:55:26 +0900

    💄 Main 페이지 UI 1차 PR

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,27 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
-import BigBanner from '../components/BigBanner/BigBanner';
-import Band from '../components/Swiper/Band';
+import { useEffect, useState } from 'react';
+import BigBanner from './components/BigBanner';
+import Band from './components/Band';
 import styled from 'styled-components';
+import GoobNews from './components/GoobNews';
+import GoobStar from './components/GoobStar';
+import Goobtube from './components/Goobtube';
 
 const Main = () => {
   /** Scroll Y값을 저장하기 위한 state */
   const [scrollY, setScrollY] = useState(0);
-
-  const isScrollY = scrollY >= window.innerHeight / 3;
-
-  // const Observer = new IntersectionObserver((entries, observer) => {
-  //   entries.forEach(entry => {
-  //     if (entry.isIntersecting) {
-  //       console.log('isIntersecting');
-  //     } else {
-  //       console.log('isNotIntersecting');
-  //     }
-  //   });
-  // });
-
-  // const section = Observer.observe(document.querySelector('section'));
-  // Observer.observe(section[0]);
-  // Observer.observe(section[1]);
 
   /**
    * useEffect를 이용하여 scroll에 대한 값을 scrollY 값이 변경될 때마다 업데이트 (의존성 배열에 scrollY를 넣어줌)
@@ -45,37 +32,23 @@ const Main = () => {
   };
 
   return (
-    <main>
-      <MainContainer>
-        <BigBannerContainer>
-          <BigBanner />
-        </BigBannerContainer>
-        <BandContainer className={isScrollY && 'pageOffset'}>
-          <Band scrollY={scrollY} />
-        </BandContainer>
-      </MainContainer>
-    </main>
+    <MainContainer>
+      <BigBanner />
+      <Band scrollY={scrollY} />
+      <GoobNews />
+      <GoobStar />
+      <Goobtube />
+    </MainContainer>
   );
 };
 
 export default Main;
 
-const MainContainer = styled.div`
+const MainContainer = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   width: 100%;
   height: 100%;
-`;
-
-const BigBannerContainer = styled.section`
-  width: 100%;
-  height: 100vh;
-`;
-
-const BandContainer = styled.section`
-  width: 100%;
-  height: 100vh;
-  transition: all 0.3s ease-in-out;
 `;
