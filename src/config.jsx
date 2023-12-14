53d4c90cb193708716ec993147b5b610afdcae58
박요진 <syon013@naver.com>
2023-12-14 20:43:52 +0900

    🎨 Axios용 config 파일 업데이트

--- src/config.jsx
+++ src/config.jsx
@@ -6,4 +6,5 @@ export const API = {
   BIG_BANNER: `${BASE_URL}/BigBannerData.json`,
   GOOB_NEWS: `${BASE_URL}/GoobNewsData.json`,
   GOOBSTAR: `${BASE_URL}/GoobStarData.json`,
+  GOOBTUBE: `${BASE_URL}/GoobTubeData.json`,
 };
