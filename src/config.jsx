59d1152b4d60c9315a78c5974e7706815777b5c8
박요진 <syon013@naver.com>
2023-12-15 11:06:20 +0900

    🎨 API key 명칭 수정

--- src/config.jsx
+++ src/config.jsx
@@ -3,7 +3,7 @@ export const API = {
   BAND_SWIPER: `/MainBandData.json`,
   BIG_BANNER: `/BigBannerData.json`,
   GOOB_NEWS: `/GoobNewsData.json`,
-  GOOBSTAR: `/GoobStarData.json`,
-  GOOBTUBE: `/GoobTubeData.json`,
+  GOOB_STAR: `/GoobStarData.json`,
+  GOOB_TUBE: `/GoobTubeData.json`,
   NAV: `/NavListData.json`,
 };
