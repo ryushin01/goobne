9b62048b4b7693e16ae78453082d96d78cb61fc9
leechengwon <141094801+leechengwon@users.noreply.github.com>
2023-12-22 19:34:55 +0900

    리스트 페이지 기능 개발 PR

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,32 +1,55 @@
 import { useEffect, useState } from 'react';
-import ListItem from './components/ListItem';
+import ItemComponent from './components/ItemComponent';
 import MenuChipGroup from '../../components/Chip/MenuChipGroup';
 import { customAxios } from '../../API/API';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const List = () => {
-  const [productList, setProductList] = useState('');
+  /** 프로덕트 리스트를 데이터를 담을 useState를 정의합니다. */
+  const [productListData, setProductListData] = useState('');
 
+  /**
+   * useNavigate()를 navigate 변수에 담습니다.
+   */
+  const navigate = useNavigate();
+
+  /**
+   * 1.chip버튼이 클릭되면 이벤트인자로 chip에 category를 인자로 받습니다.
+   * 2.axios get메서드가 카테고리에 맞는 json목데이터를 불러옵니다.
+   * 3.response 데이터를 productList useState를 담습니다.
+   * 아래 useEffect 확인하세요.
+   */
   const chipSelect = category => {
-    customAxios //eslint-disable-line no-unused-vars
+    customAxios
       .get(`/ListItem${category}.json`)
       .then(response => {
-        setProductList(response.data.result);
+        setProductListData(response.data.result);
       })
       .catch(error => {
         console.log(error);
       });
   };
 
-  const cartClick = id => {
+  /**
+   * 1.클릭이벤트 감지될때마다 실행되는 함수입니다.
+   * 2.아이템에 고유에 id를 인자로 받습니다.
+   * 3.아이디를 적용한 url로 네비게이트 해줍니다.
+   */
+  const listItemClick = id => {
     console.log(id);
+    navigate(`/detail/${id}`);
   };
 
+  /**
+   * 1.useEffect 최초실행됩니다.
+   * 2.productList값이 바뀔때마다 chipSelect 세터함수를 실행합니다.
+   */
   useEffect(() => {
     chipSelect('All');
   }, []);
 
-  if (!productList) return null;
+  if (!productListData) return null;
 
   return (
     <ListWrapMain>
@@ -38,20 +61,22 @@ const List = () => {
         </ButtonWrapDiv>
 
         <ListContainerUl>
-          {productList?.map(({ id, imgSrc, price, title, badge }, index) => {
-            return (
-              <ListItemLi key={index}>
-                <ListItem
-                  id={id}
-                  imgSrc={imgSrc}
-                  price={price}
-                  title={title}
-                  badge={badge}
-                  onClick={cartClick}
-                ></ListItem>
-              </ListItemLi>
-            );
-          })}
+          {productListData?.map(
+            ({ id, imgSrc, price, title, badge }, index) => {
+              return (
+                <ListItemLi key={index}>
+                  <ItemComponent
+                    id={id}
+                    imgSrc={imgSrc}
+                    price={price}
+                    title={title}
+                    badge={badge}
+                    onClick={listItemClick}
+                  ></ItemComponent>
+                </ListItemLi>
+              );
+            },
+          )}
         </ListContainerUl>
       </ListContainerSection>
     </ListWrapMain>
@@ -71,7 +96,7 @@ const ListContainerSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 1200px;
+  width: 1300px;
   margin: 0 auto;
   & > h2 {
     font-weight: 900;
@@ -81,14 +106,53 @@ const ListContainerSection = styled.section`
 
 const ListContainerUl = styled.ul`
   display: grid;
+  justify-content: center;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   column-gap: 10px;
   row-gap: 50px;
   margin-bottom: 200px;
+  @media (max-width: 1300px) {
+    grid-template-columns: 1fr 1fr; /* 각 아이템이 한 열에 하나씩 쌓이도록 설정 */
+  }
 `;
+
 const ListItemLi = styled.li`
   display: flex;
   justify-content: center;
+
+  &:first-child {
+    grid-column: span 2;
+    grid-row: span 2;
+    /* display: none; */
+    .emphasisContainer {
+      width: 100%;
+    }
+    .emphasisImgInner {
+      width: 100%;
+      height: 650px;
+    }
+    .emphasisTitleInner {
+      font-size: 50px;
+    }
+
+    .emphasisTitlePriceWrap {
+      width: 100%;
+    }
+    .emphasisPriceInner {
+      font-size: 30px;
+    }
+    .emphasisCartIconBtn {
+      width: 40px;
+      position: absolute;
+      right: 15px;
+      top: 10px;
+    }
+    .emphasisBadge {
+      & > span {
+        font-size: 20px;
+      }
+    }
+  }
 `;
 
 const ButtonWrapDiv = styled.div`
---
+++ src/pages/List/components/ItemComponent.jsx
@@ -0,0 +1,143 @@
+import { useNavigate } from 'react-router-dom';
+import Badge from '../../../components/Badge/Badge';
+import IconButton from '../../../components/IconButton/IconButton';
+import styled from 'styled-components';
+
+const ListItem = ({ id, imgSrc, price, title, onClick, badge, ...props }) => {
+  /**로컬스토리에서 accessToken을 token 변수에 담습니다. */
+  const token = localStorage.getItem('accessToken');
+
+  const cartIconClick = id => {
+    if (!token) {
+      navigate('/login');
+    } else {
+      navigate('/delivery');
+      console.log(id);
+    }
+  };
+
+  /**
+   * useNavigate()를 navigate 이름으로 변수로 지정합니다.
+   */
+  const navigate = useNavigate();
+  return (
+    <ListItemDiv className="emphasisContainer" {...props}>
+      <ImgInnerBtnButton
+        className="emphasisImgInner"
+        onClick={() => {
+          onClick(id);
+        }}
+      >
+        <img src={imgSrc} alt="메뉴사진" />
+      </ImgInnerBtnButton>
+
+      <ListInfoWrapDiv>
+        <BadgeWrapDiv className="test">
+          {badge?.map((badge, index) => {
+            return (
+              <Badge
+                key={index}
+                shape={badge}
+                size="small"
+                className="emphasisBadge"
+              />
+            );
+          })}
+        </BadgeWrapDiv>
+
+        <TitlePriceWrapDiv className="emphasisTitlePriceWrap">
+          <h3>
+            <TitleBtn
+              className="emphasisTitleInner"
+              onClick={() => {
+                onClick(id);
+              }}
+            >
+              {title}
+            </TitleBtn>
+          </h3>
+
+          <PriceButton
+            className="emphasisPriceInner"
+            onClick={() => {
+              onClick(id);
+            }}
+          >
+            {price.toLocaleString('ko-KR')}원
+          </PriceButton>
+          <IconButton
+            content="cart"
+            onClick={() => {
+              cartIconClick(id);
+            }}
+            className="emphasisCartIconBtn"
+          />
+        </TitlePriceWrapDiv>
+      </ListInfoWrapDiv>
+    </ListItemDiv>
+  );
+};
+
+export default ListItem;
+
+const ListItemDiv = styled.div`
+  width: 300px;
+`;
+const ListInfoWrapDiv = styled.div`
+  background-color: transparent;
+  & > h3 {
+    font-weight: 900;
+    cursor: pointer;
+  }
+  .cartIconBtn {
+    width: 25px;
+    /* position: absolute;
+    right: 10px;
+    top: -15px; */ //이전방법!
+  }
+`;
+const ImgInnerBtnButton = styled.button`
+  width: 300px;
+  height: 300px;
+  border: none;
+  cursor: pointer;
+
+  & > img {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    aspect-ratio: 1/1;
+  }
+`;
+const BadgeWrapDiv = styled.div`
+  display: flex;
+  gap: 5px;
+  padding: 10px 0px;
+`;
+
+const PriceButton = styled.button`
+  border: none;
+  background-color: transparent;
+  font-size: 16px;
+  cursor: pointer;
+
+  & + div {
+    position: absolute;
+    right: 10px;
+    top: 10px; //선택자 방법
+  }
+`;
+const TitleBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  font-size: 22px;
+  font-weight: 700;
+  cursor: pointer;
+`;
+
+const TitlePriceWrapDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  position: relative;
+`;
